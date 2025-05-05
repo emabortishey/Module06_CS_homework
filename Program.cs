7f9cc4998ccec20f8c4dbe723f6ddfbb2b6efde1
fartb <fartbreather228@gmail.com>
2025-05-05 22:13:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+public class MyException : Exception
+{
+    DateTime _time_exc;
+
+    public DateTime Time_exc { get; private set; }
+
+    public MyException() : base("My exception was called")
+    {
+        _time_exc = DateTime.Now;
+    }
+
+    public MyException(string message) : base(message) { }
+    public MyException(string message, Exception innerException) : base(message, innerException) { }
+    protected MyException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
